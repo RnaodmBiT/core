c3de1705a9b36bd6ec37298652313d12e20d878b
Ben Mitchell <rnaodm5@gmail.com>
2016-10-22 19:01:05 +1300

    Add basic text file reading.

---
+++ include/file.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+#include <linkage.hpp>
+
+namespace tk {
+    namespace core {
+
+        TK_API std::string readFile(const std::string& filename);
+
+    }
+}
---
+++ src/file.cpp
@@ -0,0 +1,21 @@
+#include <file.hpp>
+#include <fstream>
+
+namespace tk {
+    namespace core {
+
+        std::string readFile(const std::string& filename) {
+            std::ifstream t(filename);
+            std::string str;
+
+            t.seekg(0, std::ios::end);
+            str.reserve(t.tellg());
+            t.seekg(0, std::ios::beg);
+
+            str.assign((std::istreambuf_iterator<char>(t)),
+                        std::istreambuf_iterator<char>());
+            return str;
+        }
+
+    }
+}
