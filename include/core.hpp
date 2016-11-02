368ff8c90e0d3cb9add51e351daea823f9591743
Ben Mitchell <rnaodm5@gmail.com>
2016-11-03 12:18:39 +1300

    Add some general documentation to two of the headers.

--- include/core.hpp
+++ include/core.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+/**
+ * Core.hpp
+ * A general header which includes all of the individual core modules for convinience.
+ */
+
 
 #include <file.hpp>
 #include <vector.hpp>
--- include/file.hpp
+++ include/file.hpp
@@ -6,6 +6,11 @@
 namespace tk {
     namespace core {
 
+        /**
+         * Read in a text file and return its entire contents as a string.
+         * \param filename The file to read the contents from.
+         * \return The contents of the specified file. Empty if the file does not exist.
+         */
         TK_CORE std::string readFile(const std::string& filename);
 
     }
